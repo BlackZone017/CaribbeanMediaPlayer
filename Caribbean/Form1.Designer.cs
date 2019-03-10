14dd7e08b12c31a9ddb4085a6414269dce97b38a
BlackZone017 <20175184@itla.edu.do>
2019-03-10 00:50:36 -0400

    First Commit

---
+++ Caribbean/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace Caribbean
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirCtrlOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeF1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.volumeBar = new System.Windows.Forms.TrackBar();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lista = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.barMinutos = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barMinutos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(17, 48);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(784, 434);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer1_PlayStateChange);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.ayudaToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1040, 27);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirCtrlOToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(67, 23);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirCtrlOToolStripMenuItem
+            // 
+            this.abrirCtrlOToolStripMenuItem.Name = "abrirCtrlOToolStripMenuItem";
+            this.abrirCtrlOToolStripMenuItem.Size = new System.Drawing.Size(108, 24);
+            this.abrirCtrlOToolStripMenuItem.Text = "Abrir";
+            this.abrirCtrlOToolStripMenuItem.Click += new System.EventHandler(this.abrirCtrlOToolStripMenuItem_Click);
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.acercaDeF1ToolStripMenuItem});
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(60, 23);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // acercaDeF1ToolStripMenuItem
+            // 
+            this.acercaDeF1ToolStripMenuItem.Name = "acercaDeF1ToolStripMenuItem";
+            this.acercaDeF1ToolStripMenuItem.Size = new System.Drawing.Size(139, 24);
+            this.acercaDeF1ToolStripMenuItem.Text = "Acerca De";
+            this.acercaDeF1ToolStripMenuItem.Click += new System.EventHandler(this.acercaDeF1ToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(46, 23);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.volumeBar);
+            this.panel1.Controls.Add(this.btnNext);
+            this.panel1.Controls.Add(this.btnPrev);
+            this.panel1.Controls.Add(this.btnPlay);
+            this.panel1.Location = new System.Drawing.Point(17, 505);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 65);
+            this.panel1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "00:00:00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(563, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Vol.";
+            // 
+            // volumeBar
+            // 
+            this.volumeBar.AutoSize = false;
+            this.volumeBar.BackColor = System.Drawing.Color.White;
+            this.volumeBar.LargeChange = 3;
+            this.volumeBar.Location = new System.Drawing.Point(592, 24);
+            this.volumeBar.Name = "volumeBar";
+            this.volumeBar.Size = new System.Drawing.Size(162, 31);
+            this.volumeBar.TabIndex = 9;
+            this.volumeBar.Tag = "Vol.";
+            this.volumeBar.TickFrequency = 10;
+            this.volumeBar.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.volumeBar.Value = 10;
+            this.volumeBar.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // btnNext
+            // 
+            this.btnNext.AutoSize = true;
+            this.btnNext.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnNext.BackColor = System.Drawing.Color.White;
+            this.btnNext.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnNext.FlatAppearance.BorderSize = 0;
+            this.btnNext.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.btnNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNext.Image = ((System.Drawing.Image)(resources.GetObject("btnNext.Image")));
+            this.btnNext.Location = new System.Drawing.Point(419, 17);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(31, 31);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.AutoSize = true;
+            this.btnPrev.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnPrev.BackColor = System.Drawing.Color.White;
+            this.btnPrev.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnPrev.FlatAppearance.BorderSize = 0;
+            this.btnPrev.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.btnPrev.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrev.Image = ((System.Drawing.Image)(resources.GetObject("btnPrev.Image")));
+            this.btnPrev.Location = new System.Drawing.Point(320, 17);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(31, 31);
+            this.btnPrev.TabIndex = 1;
+            this.btnPrev.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnPrev.UseVisualStyleBackColor = false;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.AutoSize = true;
+            this.btnPlay.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnPlay.BackColor = System.Drawing.Color.White;
+            this.btnPlay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnPlay.FlatAppearance.BorderSize = 0;
+            this.btnPlay.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlay.Image = global::Caribbean.Properties.Resources.btnPause;
+            this.btnPlay.Location = new System.Drawing.Point(357, 4);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(56, 56);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnAgregar.FlatAppearance.BorderSize = 2;
+            this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.ForeColor = System.Drawing.Color.White;
+            this.btnAgregar.Location = new System.Drawing.Point(821, 533);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(156, 36);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar Archivo";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // lista
+            // 
+            this.lista.FormattingEnabled = true;
+            this.lista.Location = new System.Drawing.Point(819, 98);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(201, 420);
+            this.lista.TabIndex = 5;
+            this.lista.SelectedIndexChanged += new System.EventHandler(this.lista_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(824, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Lista de Reproducción";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.AutoSize = true;
+            this.btnEliminar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnEliminar.BackColor = System.Drawing.Color.White;
+            this.btnEliminar.FlatAppearance.BorderSize = 2;
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnEliminar.Image = ((System.Drawing.Image)(resources.GetObject("btnEliminar.Image")));
+            this.btnEliminar.Location = new System.Drawing.Point(985, 533);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(0);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(35, 35);
+            this.btnEliminar.TabIndex = 7;
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // barMinutos
+            // 
+            this.barMinutos.AutoSize = false;
+            this.barMinutos.BackColor = System.Drawing.Color.White;
+            this.barMinutos.Location = new System.Drawing.Point(17, 483);
+            this.barMinutos.Name = "barMinutos";
+            this.barMinutos.Size = new System.Drawing.Size(784, 22);
+            this.barMinutos.TabIndex = 11;
+            this.barMinutos.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.barMinutos.Scroll += new System.EventHandler(this.barMinutos_Scroll);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1040, 587);
+            this.Controls.Add(this.barMinutos);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lista);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CaribbeanMediaPlayer";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barMinutos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirCtrlOToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeF1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.TrackBar volumeBar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar barMinutos;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Caribbean/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Caribbean.Properties;
+
+namespace Caribbean
+{
+    public partial class Form1 : Form
+    {
+        private bool loadingPlayer;
+        private bool play = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Color de los botones
+            btnPlay.BackColor = Color.Transparent;
+            btnNext.BackColor = Color.Transparent;
+            btnPrev.BackColor = Color.Transparent;
+            //
+            loadingPlayer = false;
+            axWindowsMediaPlayer1.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(axWindowsMediaPlayer1_PlayStateChange);
+        }
+
+        //Metodo iniciar el timer
+        public void iniciarTiempo()
+        {
+            timer1.Interval = 1000; //Intervalo de 1s
+            timer1.Start();
+        }
+
+        //Metodo Abrir un archivo 
+        private void Abrir()
+        {
+            OpenFileDialog abrir = new OpenFileDialog();
+            abrir.FileName = "archivo nuevo";
+            abrir.Filter = "archivo mp3|*.mp3|archivo mp4|*.mp4|archivo avi|*.avi|archivo mkv|*.mkv";
+            abrir.FilterIndex = 1;
+            if (abrir.ShowDialog() == DialogResult.OK)
+            {
+                axWindowsMediaPlayer1.URL = abrir.FileName;
+                loadingPlayer = true;
+                barMinutos.Value = 0;
+                iniciarTiempo();
+            }
+        }
+
+        //Metodo Reproducir siguiente elemento del listBox
+        //Si el elemento seleccionado es el ultimo, al dar click a siguiente cambiar al primer elemento 
+        private void Siguiente()
+        {
+            if (lista.SelectedIndex == lista.Items.Count - 1)
+            {
+                lista.SelectedIndex = 0;
+                axWindowsMediaPlayer1.URL = lista.SelectedItem.ToString();
+            }
+            else
+            {
+                lista.SelectedIndex++;
+                axWindowsMediaPlayer1.URL = lista.SelectedItem.ToString();
+            }
+
+            if (!play)
+            {
+                btnPlay.Image = Resources.btnPause;
+                play = true;
+            }
+
+            loadingPlayer = true;
+            barMinutos.Value = 0;
+
+        }
+
+        //Metodo Reproducir anterior elemento del listBox
+        //Si el elemento seleccionado es el primero, al dar click a anterior cambiar al ultimo elemento 
+        private void Anterior()
+        {
+            if (lista.SelectedIndex <= 0)
+            {
+                lista.SelectedIndex = lista.Items.Count - 1;
+                axWindowsMediaPlayer1.URL = lista.SelectedItem.ToString();
+            }
+            else
+            {
+                lista.SelectedIndex--;
+                axWindowsMediaPlayer1.URL = lista.SelectedItem.ToString();
+            }
+
+            if (!play)
+            {
+                btnPlay.Image = Resources.btnPause;
+                play = true;
+            }
+
+            loadingPlayer = true;
+            barMinutos.Value = 0;
+        }
+
+        //Menu Archivo -> Abrir
+        private void abrirCtrlOToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Abrir();
+        }
+
+        //Menu Acerca De
+        private void acercaDeF1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Gouri Ramirez | 2017-5184");
+        }
+
+        //Menu Salir
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        //Boton Play/Pause
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (!play) //Si esta pausado cambiar de icono y poner play
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                btnPlay.Image = Resources.btnPause;
+                play = true;
+            }
+            else //Si esta en play cambiar de icono y poner pause
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.pause();
+                btnPlay.Image = Resources.btnPlay;
+                play = false;
+            }
+            
+        }
+
+        //Boton Anterior
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            Anterior();
+        }
+
+        //Boton Siguiente
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            Siguiente();
+        }
+
+        //Boton Agregar Archivo a la lista de reproduccion
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Abrir();
+            if (!play)
+            {
+                btnPlay.Image = Resources.btnPause;
+                play = true;
+            }
+            lista.Items.Add(axWindowsMediaPlayer1.URL.ToString());
+        }
+
+        //Boton Eliminar Elemento de la lista
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lista.Items.RemoveAt(lista.SelectedIndex);
+                lista.SelectedIndex = 0;
+                barMinutos.Value = 0;
+            }
+            catch (Exception ex) { }
+        }
+
+        
+        private void lista_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!play)
+                {
+                    btnPlay.Image = Resources.btnPause;
+                    play = true;
+                }
+                axWindowsMediaPlayer1.URL = lista.SelectedItem.ToString();
+                loadingPlayer = true;
+            }
+            catch (Exception ex){ }
+        }
+
+        
+        //Controlador del volumen (volumeBar event)
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            volumeBar.Minimum = 0;
+            volumeBar.Maximum = 100;
+            axWindowsMediaPlayer1.settings.volume = volumeBar.Value;
+        }
+
+        //Controlador de la posicion del video (barMinutos)
+        private void barMinutos_Scroll(object sender, EventArgs e)
+        {
+            //adaptar el video al valor del barMinutos
+            axWindowsMediaPlayer1.Ctlcontrols.currentPosition = barMinutos.Value;
+        }
+
+        //
+        private void axWindowsMediaPlayer1_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (loadingPlayer && e.newState == 3)
+            {
+                //Setear el valor maximo de la barra de minutos como la duracion del video
+                barMinutos.Maximum = (int)axWindowsMediaPlayer1.currentMedia.duration;
+
+                loadingPlayer = false;
+
+                //avanzar en 1 la barra
+                barMinutos.SmallChange = 1;         
+            }
+        }
+
+        //Avanzar la barra de Minutos cada segundo y mostrar el tiempo restante del video
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            double t = Math.Floor(axWindowsMediaPlayer1.currentMedia.duration - axWindowsMediaPlayer1.Ctlcontrols.currentPosition);
+
+            //Convertir los segundos (t) de tipo double a Timespan, y luego a String
+            string str = Convert.ToString(TimeSpan.FromSeconds(t));
+            
+            // Display the time remaining in the current media.
+            label3.Text = ("Time remaining: " + str);
+
+            //avanzar el puntero de la barra de minutos conforme avance el video
+            barMinutos.Value = (int)axWindowsMediaPlayer1.Ctlcontrols.currentPosition;
+        }
+
+    }
+}
